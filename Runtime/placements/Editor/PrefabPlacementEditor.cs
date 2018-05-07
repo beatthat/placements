8858d489f76aed2fe2f68efa9bffaa4c8d597ee2
larrykirschner <larrykirschner@gmail.com>
2018-05-06 22:35:10 -0700

    updated to support nested prefabs with safe apply (via manage-prefab-instances pkg)

---
+++ Runtime/placements/Editor/PrefabPlacementEditor.cs
@@ -0,0 +1,38 @@
+using UnityEngine;
+using UnityEditor;
+using BeatThat.ManagePrefabInstances;
+
+namespace BeatThat
+{
+	[CustomEditor(typeof(PrefabPlacement), true)]
+	[CanEditMultipleObjects]
+	public class PrefabPlacementEditor : UnityEditor.Editor 
+	{
+
+		override public void OnInspectorGUI()
+		{
+			var bkgColorSave = GUI.backgroundColor;
+			var fgColorSave = GUI.color;
+
+			PrefabPlacement p = (target as PrefabPlacement);
+
+			if (!p.isPrefabSet) {
+				GUI.backgroundColor = Color.yellow;
+				EditorGUILayout.HelpBox ("Required 'Prefab' property is not assigned", MessageType.Warning);
+			}
+
+
+			base.OnInspectorGUI();
+
+			GUI.backgroundColor = bkgColorSave;
+
+			if (!p.isPrefabSet) {
+				return;
+			}
+
+			p.OnInspectorGUI_EditPrefabs ();
+
+			GUI.backgroundColor = bkgColorSave;
+		}
+	}
+}
--- Runtime/placements/PrefabPlacement.cs
+++ Runtime/placements/PrefabPlacement.cs
@@ -1,35 +1,97 @@
 using UnityEngine;
 using BeatThat;
+using System;
+using UnityEngine.Serialization;
+using System.Collections.Generic;
+using BeatThat.ManagePrefabInstances;
 
 namespace BeatThat
 {
-	public abstract class PrefabPlacement : MonoBehaviour
+	/// <summary>
+	/// Non-generic base class for PrefabPlacement, makes it possible to have a default inspector (that's also why the class is not abstract)
+	/// </summary>
+	public class PrefabPlacement : MonoBehaviour, ManagesPrefabInstances
 	{
-		abstract public bool isPrefabSet { get; } 
-
-		abstract public bool objectExists { get; }
-
-		abstract public GameObject managedGameObject { get; }
-
-		abstract public void EnsureCreated();
-
-		abstract public void Delete(bool deleteFoundInstance);
-	}
-
-	public class PrefabPlacement<T> : PrefabPlacement, Placement<T> where T : Component
-	{
-		public T m_prefab;
 		public bool m_createObjectOnStart = false;
 		public bool m_createObjectOnEnable = false;
 		public bool m_destroyObjectOnDisable = false;
 
-		public bool m_disableWarnOnFindInstance;
+
+		[Tooltip(@"Control how you want this PrefabPlacement to treat instances of its prefab:
+	
+WarnOnFindInstance 
+------------------
+Delete instances on apply parent prefab. When the app is running, log a warning if an instance of the prefab is found. 
+
+AllowInstanceInScene
+-------------------- 
+Delete instances on apply parent prefab, but ignore instances found on the scene at runtime.
+
+AllowApplyInstanceToParentPrefab
+-------------------------------- 
+When parent prefab is applied bake any found instances into the parent prefab."
+)]
+		[FormerlySerializedAs("m_disableWarnOnFindInstance")]public PrefabInstancePolicy m_defaultInstancePolicy;
 
 
 		[Tooltip(@"set TRUE to see GizmoSettings in inspector")]
 		public bool m_unhideGizmoSettings;
 
-		override public bool isPrefabSet { get { return m_prefab != null; } }
+		public PrefabInstancePolicy defaultInstancePolicy { get { return m_defaultInstancePolicy; } } 
+
+		virtual public bool isPrefabSet { get { throw new NotImplementedException(); } } 
+
+		virtual public bool objectExists { get { throw new NotImplementedException(); } } 
+
+		virtual public GameObject managedGameObject { get { throw new NotImplementedException(); } } 
+
+		virtual public UnityEngine.Object prefabObject { get { throw new NotImplementedException(); } }
+
+		virtual public int numPrefabTypes { get { return 1; } }
+
+		virtual public void GetPrefabInstances (ICollection<PrefabInstance> instances, bool ensureCreated = false)
+		{
+			throw new NotImplementedException ();
+		}
+
+		virtual public void GetPrefabTypes (ICollection<PrefabType> types)
+		{
+			throw new NotImplementedException ();
+		}
+
+		virtual public void EnsureCreated()
+		{
+			throw new NotImplementedException();
+		}
+
+		virtual public void Delete(bool deleteFoundInstance)
+		{
+			throw new NotImplementedException();
+		}
+
+		public static void OrientToParent(Transform inst, Transform asset)
+		{
+			inst.localScale = Vector3.one;
+			inst.localRotation = Quaternion.identity;
+			inst.localPosition = Vector3.zero;
+
+			#if !(UNITY_4_3 || UNITY_4_5)
+			var rectTransform = inst as RectTransform;
+			if(rectTransform != null) {
+				RectTransform instRT = rectTransform;
+				var prefabRT = asset.transform as RectTransform;
+
+				// RectTransform objects seem to lose their offsets in instantiation?
+				// Maybe this is unity trying to scale for screen res/aspect ratio, 
+				// but it's really broken at point of this edit
+				instRT.anchorMin = prefabRT.anchorMin;
+				instRT.anchorMax = prefabRT.anchorMax;
+				instRT.offsetMin = prefabRT.offsetMin;
+				instRT.offsetMax = prefabRT.offsetMax;
+			}
+			#endif
+
+		}
 
 		#if UNITY_EDITOR
 		virtual protected void Awake()
@@ -50,6 +112,25 @@ namespace BeatThat
 		}
 		#endif
 
+		public void OnApplyPrefab_BeforeAllSiblings (GameObject prefabInstance, GameObject prefab) {}
+
+		public void OnApplyPrefab (GameObject prefabInstance, GameObject prefab)
+		{
+			#if UNITY_EDITOR
+			this.ApplyManagedPrefabInstancesThenRemoveFromParentPrefab();
+			#endif
+		}
+
+		public void OnApplyPrefab_AfterAllSiblings (GameObject prefabInstance, GameObject prefab) {}
+
+	}
+
+	public class PrefabPlacement<T> : PrefabPlacement, Placement<T> where T : Component
+	{
+		public T m_prefab;
+
+		override public bool isPrefabSet { get { return m_prefab != null; } }
+
 		// Analysis disable ConvertToAutoProperty
 		public T prefab
 		// Analysis restore ConvertToAutoProperty
@@ -61,6 +142,8 @@ namespace BeatThat
 				m_prefab = value;
 			}
 		}
+
+		public override UnityEngine.Object prefabObject { get { return this.prefab; } }
 
 		void Start()
 		{
@@ -87,7 +170,6 @@ namespace BeatThat
 		{
 			get {
 				return m_object.value != null || FindObject() != null;
-//				return m_gameObject != null || FindObject() != null;
 			}
 		}
 		
@@ -106,10 +188,36 @@ namespace BeatThat
 				EnsureCreated();
 				var o = m_object.value;
 				return o != null? o.gameObject: null;
-//				return m_gameObject;
 			}
 		}
 
+		override public void GetPrefabInstances (ICollection<PrefabInstance> instances, bool ensureCreated = false)
+		{
+			if(ensureCreated) {
+				EnsureCreated(true);
+			}
+
+			var inst = m_object.isValid? m_object.value: FindObject() as T;
+			if(inst == null) {
+				return;
+			}
+
+			instances.Add(new PrefabInstance {
+				prefab = this.prefab,
+				prefabType = typeof(T),
+				instance = inst.gameObject,
+				instancePolicy = this.defaultInstancePolicy
+			});
+		}
+
+		override public void GetPrefabTypes(ICollection<PrefabType> types)
+		{
+			types.Add (new PrefabType {
+				prefab = this.prefab,
+				prefabType = typeof(T),
+				instancePolicy = this.defaultInstancePolicy
+			});
+		}
 
 		public void Recreate()
 		{
@@ -119,19 +227,21 @@ namespace BeatThat
 
 		override public void EnsureCreated()
 		{
+			EnsureCreated (false);
+		}
+
+		private void EnsureCreated(bool supressWarnings)
+		{
 			if(m_object.value != null) {
 				return;
 			}
-//			if(m_object != null && m_gameObject != null) {
-//				return;
-//			}
 
 
 			var o = FindObject() as T;
 			if(o != null) {
 				m_foundNotInstantiated = true;
 
-				if(!m_disableWarnOnFindInstance) {
+				if(m_defaultInstancePolicy == PrefabInstancePolicy.WarnOnFindInstance && !supressWarnings) {
 					Debug.LogWarning("[" + Time.time + "] Found instance under placement prefab " 
 						+ this.Path() + ". Check 'disableWarnOnFindInstance' to disable this message.");
 				}
@@ -144,11 +254,10 @@ namespace BeatThat
 				o = NewObject() as T;
 				m_foundNotInstantiated = false;
 			}
-			
+
 			ConfigureObject(o);
 
 			m_object = new SafeRef<T>(o);
-//			m_gameObject = o.gameObject;
 		}
 
 		public void Delete()
@@ -210,11 +319,10 @@ namespace BeatThat
 			T obj = null;
 	#if UNITY_EDITOR
 			obj = !Application.isPlaying ? UnityEditor.PrefabUtility.InstantiatePrefab (m_prefab) as T : (Instantiate (m_prefab) as T);
+			obj.name = m_prefab.name;
 	#else
 			obj = (Instantiate(m_prefab) as T);
 	#endif
-
-			obj.name = this.name;
 
 			obj.transform.SetParent(this.transform, false);
 
